050f286fe7ea5c17173679c1fbb3c985dc6d87ed
marino <marinoxmatsumoto@gmail.com>
2025-10-01 10:11:34 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Iniciando navegador Firefox.
+navegador = webdriver.Firefox()
+
+# Tela cheia
+navegador.maximize_window()
+
+# link para encurtar
+url = "https://www.mercadolivre.com.br/memoria-ram-valueram-verde-4gb-1-kingston-kvr1333d3s94g-15v/p/MLB6065670?has_official_store=false&highlight=true&headerTopBrand=false#polycard_client=search-nordic&search_layout=grid&position=4&type=product&tracking_id=f63bb904-4df9-4074-975a-992019c2f36f&wid=MLB4020351181&sid=search"
+
+# Acessar a página do marketplace.
+navegador.get(url)
+
+# Tempo de espera para carregar a página.
+navegador.implicitly_wait(1.0)
+
+# Localiza a barra de pesquisa e escrever.
+#navegador.find_element("id", "cb1-edit").send_keys("Memoria RAM")
+#navegador.find_element("class name", "nav-icon-search").click()
+#navegador.find_element("title", "Memória RAM ValueRAM Verde 4GB 1 Kingston KVR1333D3S9/4G 1.5V Imagen - 1/4").click()
+
+navegador.quit()
