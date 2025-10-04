79751d7ac95e93289215012d2c1dbb3c5295383b
marino <marinoxmatsumoto@gmail.com>
2025-10-04 14:22:39 -0300

    Adicionado o .gitignore

--- main.py
+++ main.py
@@ -8,12 +8,52 @@ navegador = webdriver.Firefox()
 navegador.maximize_window()
 
 # link para encurtar
-url = "https://www.mercadolivre.com.br/memoria-ram-valueram-verde-4gb-1-kingston-kvr1333d3s94g-15v/p/MLB6065670?has_official_store=false&highlight=true&headerTopBrand=false#polycard_client=search-nordic&search_layout=grid&position=4&type=product&tracking_id=f63bb904-4df9-4074-975a-992019c2f36f&wid=MLB4020351181&sid=search"
+url = "https://www.mercadolivre.com.br/"
+
 
 # Acessar a página do marketplace.
 navegador.get(url)
 
+
 # Tempo de espera para carregar a página.
 navegador.implicitly_wait(1.0)
+
+# Localiza a barra de pesquisa e escrever.
+el = navegador.find_element(By.ID, "cb1-edit")
+el.send_keys("Livro")
+
+el = navegador.find_element(By.CLASS_NAME, "nav-icon-search")
+el.click()
+
+# Primeiro item
+navegador.find_element(By.CLASS_NAME, 'ui-search-result__wrapper').click()
+
+# Nome do produto
+nome_produto = navegador.find_element(By.CLASS_NAME, 'ui-pdp-title').text
+
+# Descrição
+descricao_produto = navegador.find_element(By.CLASS_NAME, 'ui-pdp-description__content').text
+
+# Vendedor
+vendedor_produto = navegador.find_element(By.CLASS_NAME, 'ui-seller-data-header__title').text
+
+# Preço inteiro
+preco_inteiro = navegador.find_element(By.CLASS_NAME, 'andes-money-amount__fraction').text
+
+# Preço fracionario
+preco_fracionario = navegador.find_element(By.CLASS_NAME, 'andes-money-amount__cents').text
+
+image_elmento_produto_url = navegador.find_element(By.CLASS_NAME, 'ui-pdp-image')
+imagem_produto = image_elmento_produto_url.get_attribute("src")
+
+text_file = f"""Nome do produto: {nome_produto}
+Preço do produto: R${preco_inteiro},{preco_fracionario}
+Descrição do produto: {descricao_produto}
+URL da imagem: {imagem_produto}
+Vendedor do produto: {vendedor_produto}"""
+
+with open("produto.txt", "w") as file:
+    file.write(text_file)
+
 
 navegador.quit()
